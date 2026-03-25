e1bc2895624374b8e9d9340dd72eb20898bd8076
nusrat <nusratjahan77222@gmail.com>
2026-03-25 10:19:31 +0600

    Lift up state in arrays by comparing reference

--- src/components/countries/countries.jsx
+++ src/components/countries/countries.jsx
@@ -7,7 +7,8 @@ const Countries = ({countriesPromise}) => {
     const [visitedCountries, setVisitedCountries] = useState([]);
 
     const handleVisitedCountries = (country) => {
-        console.log('handle visited country clicked', country);
+        const newVisitedCountries = [...visitedCountries, country];
+        setVisitedCountries(newVisitedCountries);
     }
 
     const countriesData = use(countriesPromise);
@@ -17,6 +18,13 @@ const Countries = ({countriesPromise}) => {
         <div>
             <h1>In the countries: {countries.length}</h1>
             <h3>Total Country Visited: {visitedCountries.length} </h3>
+            <ol>
+                {
+                    visitedCountries.map(country => <li 
+                        key={country.cca3.cca3}>
+                            {country.name.common}</li>)
+                }
+            </ol>
             <div className='countries'>
             {
                 countries.map(country => 
